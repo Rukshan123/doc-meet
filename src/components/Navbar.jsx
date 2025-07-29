874404f1b630507cabb5d8f0d78825902af7fcd7
Rukshan123 <rukzmadu@gmail.com>
2025-07-29 12:25:47 +0530

    navbar img bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from "../context/AppContext";
 const Navbar = () => {
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
-    const { token, setToken } = useContext(AppContext);
+    const { token, setToken, userData } = useContext(AppContext);
 
     const logout = () => {
         setToken(false);
@@ -35,9 +35,9 @@ const Navbar = () => {
                 </NavLink>
             </ul>
             <div className="flex items-center gap-5">
-                {token ? (
+                {token && userData ? (
                     <div className="flex items-center gap-2 cursor-pointer group relative">
-                        <img className="w-8 rounded-full" src={assets.profile_pic} alt="profilePic" />
+                        <img className="w-8 rounded-full" src={userData.image} alt="profilePic" />
                         <img className="w-2.5" src={assets.dropdown_icon} alt="dropdownIcon" />
 
                         <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
