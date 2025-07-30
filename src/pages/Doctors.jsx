1201754f7f8fbb6b46ec581133e48dd50bd90e24
Rukshan123 <rukzmadu@gmail.com>
2025-07-30 16:48:23 +0530

    bug fixed

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -12,9 +12,7 @@ const Doctors = () => {
 
     const applyFilter = () => {
         if (speciality) {
-            setFilterDoc(
-                doctors.filter((doctor) => doctor.speciality === speciality)
-            );
+            setFilterDoc(doctors.filter((doctor) => doctor.specialization === speciality));
         } else {
             setFilterDoc(doctors);
         }
@@ -35,38 +33,24 @@ const Doctors = () => {
 
     return (
         <div>
-            <p className="text-gray-600">
-                Browse through the doctors speciality.
-            </p>
+            <p className="text-gray-600">Browse through the doctors speciality.</p>
             <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
                 <button
                     className={`py-1 px-3 border rounded text-sm transition-all sm:hidden${
                         showFilter ? " bg-primary text-white" : ""
                     }`}
-                    onClick={() => setShowFilter((prev) => !prev)}
-                >
+                    onClick={() => setShowFilter((prev) => !prev)}>
                     Filters
                 </button>
 
-                <div
-                    className={` flex-col gap-4 text-sm text-gray-600 ${
-                        showFilter ? "flex" : "hidden sm:flex"
-                    }`}
-                >
+                <div className={` flex-col gap-4 text-sm text-gray-600 ${showFilter ? "flex" : "hidden sm:flex"}`}>
                     {specialities.map(({ name }) => (
                         <p
                             key={name}
-                            onClick={() =>
-                                speciality === name
-                                    ? navigate("/doctors")
-                                    : navigate(`/doctors/${name}`)
-                            }
+                            onClick={() => (speciality === name ? navigate("/doctors") : navigate(`/doctors/${name}`))}
                             className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 cursor-pointer rounded transition-all ${
-                                speciality === name
-                                    ? "bg-indigo-100 text-black"
-                                    : ""
-                            }`}
-                        >
+                                speciality === name ? "bg-indigo-100 text-black" : ""
+                            }`}>
                             {name}
                         </p>
                     ))}
@@ -75,28 +59,17 @@ const Doctors = () => {
                 <div className="w-full grid grid-cols-auto gap-4 gap-y-6">
                     {filterDoc.map((doctor, index) => (
                         <div
-                            onClick={() =>
-                                navigate(`/appointment/${doctor._id}`)
-                            }
+                            onClick={() => navigate(`/appointment/${doctor._id}`)}
                             key={index}
-                            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-                        >
-                            <img
-                                className="bg-blue-50"
-                                src={doctor.image}
-                                alt=""
-                            />
+                            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+                            <img className="bg-blue-50" src={doctor.image} alt="" />
                             <div className="p-4">
                                 <div className="flex items-center gap-2 text-sm text-center text-green-500">
                                     <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                                     <p>Available</p>
                                 </div>
-                                <p className="text-gray-900 text-lg font-medium">
-                                    {doctor.name}
-                                </p>
-                                <p className="text-gray-600 text-sm">
-                                    {doctor.speciality}
-                                </p>
+                                <p className="text-gray-900 text-lg font-medium">{doctor.name}</p>
+                                <p className="text-gray-600 text-sm">{doctor.speciality}</p>
                             </div>
                         </div>
                     ))}
